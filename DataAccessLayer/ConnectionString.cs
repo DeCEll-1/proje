8341548a0a444000f9150136bf93a1a34fd83741
DeCell_ <poedx2@gmail.com>
2022-09-24 13:30:37 +0300

    s

--- DataAccessLayer/ConnectionString.cs
+++ DataAccessLayer/ConnectionString.cs
@@ -8,6 +8,6 @@ namespace DataAccessLayer
 {
     public class ConnectionString
     {
-        public static string ConStrLocal = @"Data Source=.\SQLEXPRESS; Initial Catalog=Yemek_DB; Integrated Security=True";
+        public static string ConStrLocal = @"Data Source=DESKTOP-3RFHG0M\SQLEXPRESS; Initial Catalog=Yemek_DB; Integrated Security=True";
     }
 }
